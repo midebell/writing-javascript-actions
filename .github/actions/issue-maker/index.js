fd677108f7f14a21f28d1edeba7a42a9ddcd8373
midebell <35881982+midebell@users.noreply.github.com>
2020-09-21 17:08:39 -0500

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,11 +9,11 @@ async function run() {
 
     const octokit = new github.GitHub(token);
 
-    const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
+//     const newIssue = await octokit.issues.create({
+//       repo: github.context.repo.repo,
+//       owner: github.context.repo.owner,
+//       title: issueTitle,
+//       body: jokeBody
     });
   } catch (error) {
     core.setFailed(error.message);
